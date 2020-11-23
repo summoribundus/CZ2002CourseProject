3a997f07e802b263dc230bf406da858302492dd3
FromTheFarNorth <52026538+YuXinyang-Victor@users.noreply.github.com>
2020-11-23 23:26:28 +0800

    Update AllAdmins.java

--- src/AllAdmins.java
+++ src/AllAdmins.java
@@ -5,18 +5,40 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+/**
+Handles all the files and IO related to administrators. 
+It is a subclass of the Filehandle class.  
+@author Group3_SS6_CZ2002
+@version 1.0
+@since 2020-11-22
+*/
 class AllAdmins extends FileHandle{
+    /**
+     * This is a list that stores the admin information read from the .dat file into admin objects.  
+     * The content of the list is written to the files after operations are done. 
+     */
     private List<Admin> adminList;
 
+    /**
+     * Constructor of the AllAdmins class. 
+     */
     public AllAdmins(){
 
     }
 
+    /**
+     * Gets the full adminList
+     */
     public List<Admin> getList(){
         return adminList;
     }
 
-    public void deserializeFromFile(){
+    /**
+     * File-reading function. The file name for the admin file should be admin.dat
+     * If the process fails, stacktrace will be printed to show what went wrong. 
+     * The adminList attribute holds the data read from the file. 
+     */
+     public void deserializeFromFile(){
         List<Admin> oDetails = null;
 		FileInputStream fis = null;
 		ObjectInputStream in = null;
@@ -32,6 +54,11 @@ class AllAdmins extends FileHandle{
 		
     }
 
+    /**
+     * File-writing function. The file name for the admin file should be admin.dat
+     * If the process fails, stacktrace will be printed to show what went wrong. 
+     * The adminList attribute holds the data to be written to the file. 
+     */
     public void serializeToFile(){
         FileOutputStream fos = null;
 		ObjectOutputStream out = null;
